a4fc4ff46262aa79654eab6ed38f70bf83335148
BsmMokh <baseemm04@gmail.com>
2025-04-20 12:39:16 -0400

    Create resources.py

---
+++ resources.py
@@ -0,0 +1,19 @@
+"""
+Guide module.
+
+Provides lookup functions to query resources by name.
+"""
+
+from .resource import Resource
+
+def find_resource_info(name: str, resources: list[Resource]) -> str:
+    """
+    Search for a resource by name and return its info string.
+
+    Args:
+        name (str): The resource to look up (case‑insensitive).
+        resources (list[Resource]): Preloaded list of Resource objects.
+
+    Returns:
+        str: Description from Resource.get_info() or "Resource not found."
+    """
